bffac4a8f64604d40512c275292a5621d7d95d44
Denis Maley <malei_denis@mail.ru>
2017-02-09 23:08:58 +0300

    sortable jquery-ui added

--- public/js/home.js
+++ public/js/home.js
@@ -1,88 +1,45 @@
-(function () {
-	'use strict';
-
-	var byId = function (id) { return document.getElementById(id); },
-
-		loadScripts = function (desc, callback) {
-			var deps = [], key, idx = 0;
-
-			for (key in desc) {
-				deps.push(key);
-			}
-
-			(function _next() {
-				var pid,
-					name = deps[idx],
-					script = document.createElement('script');
-
-				script.type = 'text/javascript';
-				script.src = desc[deps[idx]];
-
-				pid = setInterval(function () {
-					if (window[name]) {
-						clearTimeout(pid);
-
-						deps[idx++] = window[name];
-
-						if (deps[idx]) {
-							_next();
-						} else {
-							callback.apply(null, deps);
-						}
-					}
-				}, 30);
-
-				document.getElementsByTagName('head')[0].appendChild(script);
-			})()
+( function() {		
+	
+	//Properties and events you can find here http://api.jqueryui.com/sortable/
+	
+	var project_limit = 5;
+	
+	//All project list 
+	$( "ul.do-drop#projects-list" ).sortable({
+		connectWith: "ul.do-drop",
+		//After returning project from list of chosen we zeroize numbering
+		receive: function(ui) {
+			$(this).find('span.project-number').html('')
 		},
-
-		console = window.console;
-
-
-	if (!console.log) {
-		console.log = function () {
-			alert([].join.apply(arguments, ' '));
-		};
-	}
-
-	// Advanced groups
-	[{
-		name: 'advanced',
-		pull: true,
-		put: true
-	},
-	{
-		name: 'advanced',
-		pull: true,
-		put: true
-	}].forEach(function (groupOpts, i) {
-		Sortable.create(byId('advanced-' + (i + 1)), {
-			sort: (i != 1),
-			group: groupOpts,
-			animation: 150
-		});
 	});
-})();
-
-// Background
-document.addEventListener("DOMContentLoaded", function () {
-	function setNoiseBackground(el, width, height, opacity) {
-		var canvas = document.createElement("canvas");
-		var context = canvas.getContext("2d");
-
-		canvas.width = width;
-		canvas.height = height;
-
-		for (var i = 0; i < width; i++) {
-			for (var j = 0; j < height; j++) {
-				var val = Math.floor(Math.random() * 255);
-				context.fillStyle = "rgba(" + val + "," + val + "," + val + "," + opacity + ")";
-				context.fillRect(i, j, 1, 1);
+	
+	//Chosen list
+	$('ul.do-drop#chosen-projects-list').sortable({
+		connectWith:'ul.do-drop',
+		//We block dropping if limit of projects is over
+		receive: function(ui) {
+			if($(this).children().length >= project_limit) {
+				$(this).addClass('dont-drop');
+				$('.do-drop').sortable('option', 'connectWith',$('.do-drop:not(.dont-drop)'));
 			}
-		}
-
-		el.style.background = "url(" + canvas.toDataURL("image/png") + ")";
-	}
-
-	setNoiseBackground(document.getElementsByTagName('body')[0], 50, 50, 0.02);
-}, false);
+		},
+		//Unblock if project is returning
+		beforeStop: function(ui) {
+			if($(this).children().length < project_limit) {
+				$(this).removeClass('dont-drop');
+				$('.do-drop').sortable('option', 'connectWith',$('.do-drop:not(.dont-drop)'));
+			}
+		},
+		//Set numbering of items
+		update: function(event, ui) {  
+			$(this).children().each(function (i) {
+                var project_num = i + 1;
+                $(this).find('span.project-number').html(project_num + '. ');
+            });
+		},
+	}).text(
+		'Drop here '+project_limit+' projects.'
+	);
+	
+	$( "ul.do-drop#projects-list, ul.do-drop#chosen-projects-list" ).disableSelection();
+} )();
